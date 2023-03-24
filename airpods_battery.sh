2127560ac0475c92d311ae32464a67c81f3b8277
Doyoon Lee <dynleedev@gmail.com>
2023-03-24 17:32:43 +0900

    change two options to three options

--- airpods_battery.sh
+++ airpods_battery.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Set your AirPods name
-airpods_name="My Airpods Pro"
+airpods_name="Your AirPods Name"
 
 # Get the current status of Bluetooth on macOS Ventura
 bluetooth_status=$(system_profiler SPBluetoothDataType | grep -c "Not Connected:" )
+num_lines_not_connected=$(system_profiler SPBluetoothDataType | grep -A 100 "Not Connected:"  | wc -l )
 
 if [ "$bluetooth_status" -eq 0 ]; then
 	# If Bluetooth is turned off
-	echo "Not Connected"
+	echo "Bluetooth OFF"
 else
-	# If Bluetooth is turned on
-	system_profiler SPBluetoothDataType | grep -A 7 "$airpods_name" | awk '/Left Battery Level/{print $4} /Right Battery Level/{print $4}' ORS=' ' \
-	&& system_profiler SPBluetoothDataType | grep -A 7 "$airpods_name" | awk '/Case Battery Level/{print "("$4 ")"}'| tail -n1
+	if [ "$num_lines_not_connected" -eq 5 ]; then
+		# If Bluetooth is turned on & AirPods is connected
+		system_profiler SPBluetoothDataType | grep -A 7 "$airpods_name" | awk '/Left Battery Level/{print $4} /Right Battery Level/{print $4}' ORS=' ' \
+		&& system_profiler SPBluetoothDataType | grep -A 7 "$airpods_name" | awk '/Case Battery Level/{print "("$4 ")"}'| tail -n1
+	else
+		# If Bluetooth is turned on & AirPods is not connected
+		echo "Not Connected"
+	fi
 fi
